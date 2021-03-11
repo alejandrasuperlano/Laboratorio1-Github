7dd2e6c16570c25bf18258334898848d50476ad7
Alejandra Superlano <rsuperlano@unal.edu.co>
2021-03-10 19:22:52 -0500

    - Cambio de nombre de clase Functions a Suma - Modificaciones en la clase Suma y en el main

--- src/main.java
+++ src/main.java
@@ -5,5 +5,7 @@ public class main {
     public static void main(String[] args) {
         Chanchito chanchito = new Chanchito();
         chanchito.callChanchito();
+        Suma suma = new Suma();
+        System.out.println(suma.sumar(3,2));
     }
 }
